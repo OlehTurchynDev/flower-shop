f511ec9c57169a6f13d1490a3bfc918489921e99
Oleh Turchyn <olehturchyndev@gmail.com>
2025-11-12 00:43:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flower-shop',
+  webDir: 'www'
+};
+
+export default config;
